262befacca455621dad0b185613ca9c23638159d
whitebird2625 <47933463+whitebird2625@users.noreply.github.com>
2020-01-09 21:44:27 -0800

    ADD

--- MaxAr.cpp
+++ MaxAr.cpp
@@ -19,8 +19,49 @@ int MaxAr(int arr[], int size)
 			max = arr[index];
 		}
 	}
-
 	return max;
 }
+
+/**************************************
+ * for(dateType identifier : arrName)
+ * 	{
+ * 		statement
+ * 	}
+ * ----------------------------------
+ * This way can not use in function,
+ * because list pass by reference(address),not array
+ * but could use in main.cpp
+ *************************************/
+//		for (int num : arr)
+//		{
+//			if(max < num)
+//			{
+//				max = num;
+//			}
+//		}
+
+/**************************************
+ * Recursion
+ *************************************/
+//		int MaxFind (int list[],int lowIndex, int upperIndex)
+//		{
+//			int max;
+//
+//			if (lowIndex == upperIndex)
+//			{
+//				return list[lowIndex];
+//			}
+//			else
+//			{
+//				max = MaxFind(list,lowIndex +1,upperIndex);
+//				if (list[lowIndex] >= max)
+//					return list[lowIndex];
+//				else
+//				{
+//					return max;
+//				}
+//			}
+//		}
+
 
 
